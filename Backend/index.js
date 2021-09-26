b0ba6de14d0ce90d5e83cefa11615de401d19307
akanniquadree <78854242+akanniquadree@users.noreply.github.com>
2021-09-26 11:32:21 +0100

    Slight changes

--- Backend/index.js
+++ Backend/index.js
@@ -48,7 +48,7 @@ app.use("/api/auth", router);
  if(process.env.NODE_ENV === "production"){
      app.use(express.static("frontend/build"))
      app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(__dirname, "client", 'build', "index.html"))
+        res.sendFile(path.resolve(__dirname, "frontend", 'build', "index.html"))
      })
  }
 
