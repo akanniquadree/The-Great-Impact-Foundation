2e0992654a4228d45838d9383dd2455cbfa9a7fa
akanniquadree <78854242+akanniquadree@users.noreply.github.com>
2021-09-27 22:12:07 +0100

    start script added

--- Backend/index.js
+++ Backend/index.js
@@ -48,8 +48,10 @@ app.use("/api/auth", router);
  
  if(process.env.NODE_ENV === 'production'){
      app.use(express.static(path.join(__dirname, '../build')))
+
      app.get("*", (req, res)=>{
         res.sendFile(path.join(__dirname, '../build'))
+     })
  }
 
 
