5a8357cdd2729e5fdbe90fa819ea2fff7d286784
akanniquadree <78854242+akanniquadree@users.noreply.github.com>
2021-10-03 23:12:43 +0100

    slight changes

--- Backend/index.js
+++ Backend/index.js
@@ -53,7 +53,7 @@ app.use("/api/auth", router);
      app.use(express.static(path.join(__dirname, "frontend", "build")))
 
      app.get("*", (req, res)=>{
-        res.sendFile(path.join(__dirname, "frontend", "build", ))
+        res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
      })
  }
 
