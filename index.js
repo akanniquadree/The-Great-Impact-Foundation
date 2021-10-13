d110143a768ae5911998edd31975b09d8194b261
akanniquadree <78854242+akanniquadree@users.noreply.github.com>
2021-10-13 12:21:35 +0100

    slight changes

--- index.js
+++ index.js
@@ -45,11 +45,9 @@ app.use("/api/auth", router);
  app.use("/api/gallery", galleryRouter);
 
 
- app.use(express.static(path.join(__dirname, "frontend", "build")))
-
- app.get("*", (req, res) => {
-     res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
- });
+if(process.env.NODE_ENV === "production"){
+   app.use(express.static("frontend/build"))
+}
 
 
 
